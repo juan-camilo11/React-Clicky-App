0bf6cf656d443693aa9f4399b977b1377990f32c
Juan Varon <juancamilo_11@utexas.edu>
2018-10-18 19:25:57 -0500

    Updates

---
+++ precache-manifest.15ad0de2c46d5a4830ae201d26c1af47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a407dcecf7ce3771b571",
+    "url": "/React-Clicky-App/static/js/runtime~main.a407dcec.js"
+  },
+  {
+    "revision": "4da8de15eabca4b5312e",
+    "url": "/React-Clicky-App/static/js/main.4da8de15.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/React-Clicky-App/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "4da8de15eabca4b5312e",
+    "url": "/React-Clicky-App/static/css/main.16199746.chunk.css"
+  },
+  {
+    "revision": "edade483633a3947094cf3bd70edd93c",
+    "url": "/React-Clicky-App/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b623b0c4fe186ec96ec27c92b81f4dea.js"
+  "/React-Clicky-App/precache-manifest.15ad0de2c46d5a4830ae201d26c1af47.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/React-Clicky-App/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
