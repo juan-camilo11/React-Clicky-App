f9679cb531a66d4db2e2877b48118c333c8274df
Juan Varon <juancamilo_11@utexas.edu>
2018-10-18 19:01:13 -0500

    Updates

---
+++ precache-manifest.b623b0c4fe186ec96ec27c92b81f4dea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e2826f58d2ae98c7cfe1",
+    "url": "/static/js/main.e2826f58.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "e2826f58d2ae98c7cfe1",
+    "url": "/static/css/main.0d768ebb.chunk.css"
+  },
+  {
+    "revision": "ffbbeeeaadb1fe7804b6062a646a486b",
+    "url": "/index.html"
+  }
+];
